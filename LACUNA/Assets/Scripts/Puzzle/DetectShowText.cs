093639f5dbac7c84c0a61866f774ad26da15723a
KK44689 <61814979+KK44689@users.noreply.github.com>
2021-12-04 17:38:38 +0700

    add star puzzle -- chap2
    - can solved star puzzle
    - disable puzzle after puzzle solved
    - add camera follow pan -- Chapter 2
    - move star puzzle scripts to BookStore_Puzzle folder

---
+++ LACUNA/Assets/Scripts/Puzzle/DetectShowText.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectShowText : MonoBehaviour
+{
+    public GameObject pickUpText;
+
+    public GameObject Puzzle;
+
+    public static bool showPuzzle = false;
+
+    // public GameObject Puzzle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickUpText.SetActive(false);
+        Puzzle.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (
+            showPuzzle &&
+            Input.GetKeyDown(KeyCode.E)
+        )
+        {
+            pickUpText.SetActive(false);
+            Puzzle.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (
+            collision.gameObject.tag.Equals("Player")
+        )
+        {
+            pickUpText.SetActive(true);
+            showPuzzle = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            pickUpText.SetActive(false);
+            showPuzzle = false;
+            Puzzle.SetActive(false);
+        }
+    }
+}
